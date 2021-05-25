2375fec200b508c8b8265f258dee683bf2db3c12
mbaiza27 <mbaiza@DESKTOP-4ML67VE.localdomain>
2021-05-24 17:27:10 -0700

    Added basic functionality to pre-processing step, not yet complete.

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ import math
 ######################
 #   Pre-Processing   #
 ######################
+def pre_proccessing():
+    print("Start pre-processing data...\n")
 
+    trainingSet = "trainingSet.txt"
+
+    #Gets the training data from the provided text file
+    trainingData = get_training_data(trainingSet)
+
+def get_training_data(trainingFileName):
+    print("Retrieving training data...")
 
 ######################
 #   Classification   #
